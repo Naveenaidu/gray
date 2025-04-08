1d4a1ebc363963f2467e4cb0ed33f37aa34b1f93
Naveen Naidu <naveennaidu479@gmail.com>
2025-04-08 09:57:58 +0530

    initial code

---
+++ ray_tracer.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"log"
+
+	"rsc.io/quote"
+)
+
+func Hello(name string) (string, error) {
+	if name == "" {
+		return "", errors.New("empty name")
+	}
+	message := fmt.Sprintf("Hi, %v. Welcome, name", name)
+	return message, nil
+}
+
+func main() {
+	// Set properties of the predefined Logger, including
+	// the log entry prefix and a flag to disable printing
+	// the time, source file, and line number.
+	log.SetPrefix("ray_tracer: ")
+	log.SetFlags(0)
+
+	message, err := Hello("")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(message)
+	fmt.Println(quote.Glass())
+}
