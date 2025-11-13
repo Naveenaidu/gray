e4562886baa251b7e13d4fa44ddb12ef8fdc00db
Naveen Naidu <naveennaidu479@gmail.com>
2025-11-13 14:39:06 +0530

    chap 7: wip - camera pixelSize

--- src/scene/camera.go
+++ src/scene/camera.go
@@ -1,15 +1,19 @@
 package scene
 
-import "github.com/Naveenaidu/gray/src/core/math"
+import (
+	"math"
+
+	coreMath "github.com/Naveenaidu/gray/src/core/math"
+)
 
 // transformation matrix that orients the world relative to the eye
-func ViewTransform(from math.Point, to math.Point, up math.Vector) *math.Matrix {
+func ViewTransform(from coreMath.Point, to coreMath.Point, up coreMath.Vector) *coreMath.Matrix {
 	forward := to.Subtract(from).Normalize()
 	upNormalized := up.Normalize()
 	left := forward.CrossProduct(*upNormalized)
 	trueUp := left.CrossProduct(*forward)
 
-	orientation := math.NewMatrix(4, 4, [][]float64{
+	orientation := coreMath.NewMatrix(4, 4, [][]float64{
 		{left.X, left.Y, left.Z, 0},
 		{trueUp.X, trueUp.Y, trueUp.Z, 0},
 		{-forward.X, -forward.Y, -forward.Z, 0},
@@ -17,7 +21,7 @@ func ViewTransform(from math.Point, to math.Point, up math.Vector) *math.Matrix 
 	})
 
 	// move the scene into place before orienting it
-	translationM := math.TranslationM(-from.X, -from.Y, -from.Z)
+	translationM := coreMath.TranslationM(-from.X, -from.Y, -from.Z)
 	return orientation.Multiply(*translationM)
 }
 
@@ -29,14 +33,38 @@ type Camera struct {
 	// angle that describes how much the camera can see
 	FieldOfView float64
 	// matrix representing how the world should be oriented relative to the camera
-	Transform math.Matrix
+	Transform coreMath.Matrix
+	// size of the pixel on the canvas
+	PixelSize  float64
+	HalfWidth  float64
+	halfHeight float64
 }
 
 func NewCamera(hsize int, vsize int, fieldOfView float64) *Camera {
+
+	// calculate the pixel size
+	halfView := math.Tan(fieldOfView / 2)
+	aspect := hsize / vsize
+
+	var halfWidth, halfHeight float64
+
+	if aspect >= 1 {
+		halfWidth = halfView
+		halfHeight = halfView / float64(aspect)
+	} else {
+		halfWidth = halfView * float64(aspect)
+		halfHeight = halfView
+	}
+
+	pixelSize := halfWidth * 2 / float64(hsize)
+
 	return &Camera{
 		Hsize:       hsize,
 		Vsize:       vsize,
 		FieldOfView: fieldOfView,
-		Transform:   *math.IdentityMatrix(),
+		Transform:   *coreMath.IdentityMatrix(),
+		PixelSize:   pixelSize,
+		HalfWidth:   halfWidth,
+		halfHeight:  halfHeight,
 	}
 }
